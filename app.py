0bb0c1ac7940db8d0a6ece2b9b23e3347af0c1bd
adii0409 <107432796+adii0409@users.noreply.github.com>
2023-05-16 13:31:15 +0700

    replace app.py

--- app.py
+++ app.py
@@ -28,14 +28,14 @@ options_age = ['18-30', '31-50', 'Over 51', 'Unknown', 'Under 18']
 # hour of the day: range of 0 to 23
 
 options_types_collision = ['Vehicle with vehicle collision','Collision with roadside objects',
-              'Collision with pedestrians','Rollover','Collision with animals',
-              'Unknown','Collision with roadside-parked vehicles','Fall from vehicles',
-              'Other','With Train']
+    'Collision with pedestrians','Rollover','Collision with animals',
+    'Unknown','Collision with roadside-parked vehicles','Fall from vehicles',
+    'Other','With Train']
 
 options_sex = ['Male','Female','Unknown']
 
 options_education_level = ['Junior high school','Elementary school','High school',
-              'Unknown','Above high school','Writing & reading','Illiterate']
+    'Unknown','Above high school','Writing & reading','Illiterate']
 
 options_services_year = ['Unknown','2-5yrs','Above 10yr','5-10yrs','1-2yr','Below 1yr']
 
@@ -55,51 +55,51 @@ st.markdown("<h1 style='text-align: center;'>Accident Severity Prediction App �
 # define a main() function to take inputs from user in form based approach
 def main():
     with st.form("road_traffic_severity_form"):
-       st.subheader("Please enter the following inputs:")
-        
-       No_vehicles = st.slider("Number of vehicles involved:",1,7, value=0, format="%d")
-       No_casualties = st.slider("Number of casualties:",1,8, value=0, format="%d")
-       Hour = st.slider("Hour of the day:", 0, 23, value=0, format="%d")
-       collision = st.selectbox("Type of collision:",options=options_types_collision)
-       Age_band = st.selectbox("Driver age group?:", options=options_age)
-       Sex = st.selectbox("Sex of the driver:", options=options_sex)
-       Education = st.selectbox("Education of driver:",options=options_education_level)
-       service_vehicle = st.selectbox("Service year of vehicle:", options=options_services_year)
-       Day_week = st.selectbox("Day of the week:", options=options_day)
-       Accident_area = st.selectbox("Area of accident:", options=options_acc_area)
-        
-       submit = st.form_submit_button("Predict")
+        st.subheader("Please enter the following inputs:")
+            
+        No_vehicles = st.slider("Number of vehicles involved:",1,7, value=0, format="%d")
+        No_casualties = st.slider("Number of casualties:",1,8, value=0, format="%d")
+        Hour = st.slider("Hour of the day:", 0, 23, value=0, format="%d")
+        collision = st.selectbox("Type of collision:",options=options_types_collision)
+        Age_band = st.selectbox("Driver age group?:", options=options_age)
+        Sex = st.selectbox("Sex of the driver:", options=options_sex)
+        Education = st.selectbox("Education of driver:",options=options_education_level)
+        service_vehicle = st.selectbox("Service year of vehicle:", options=options_services_year)
+        Day_week = st.selectbox("Day of the week:", options=options_day)
+        Accident_area = st.selectbox("Area of accident:", options=options_acc_area)
+            
+        submit = st.form_submit_button("Predict")
 
 # encode using ordinal encoder and predict
     if submit:
-       input_array = np.array([collision,
-                  Age_band,Sex,Education,service_vehicle,
-                  Day_week,Accident_area], ndmin=2)
-        
-       encoded_arr = list(encoder.transform(input_array).ravel())
-        
-       num_arr = [No_vehicles,No_casualties,Hour]
-       pred_arr = np.array(num_arr + encoded_arr).reshape(1,-1)        
-      
-# predict the target from all the input features
-       prediction = model.predict(pred_arr)
-        
-       if prediction == 0:
-           st.write(f"The severity prediction is fatal injury⚠")
-       elif prediction == 1:
-           st.write(f"The severity prediction is serious injury")
-       else:
-           st.write(f"The severity prediction is slight injury")
-        
-       st.write("Developed By: Avi kumar Talaviya")
-       st.markdown("""Reach out to me on: [Twitter](https://twitter.com/avikumart_) |
-       [Linkedin](https://www.linkedin.com/in/avi-kumar-talaviya-739153147/) |
-       [Kaggle](https://www.kaggle.com/avikumart) 
-       """)
+        input_array = np.array([collision,
+                    Age_band,Sex,Education,service_vehicle,
+                    Day_week,Accident_area], ndmin=2)
+            
+        encoded_arr = list(encoder.transform(input_array).ravel())
+            
+        num_arr = [No_vehicles,No_casualties,Hour]
+        pred_arr = np.array(num_arr + encoded_arr).reshape(1,-1)        
 
-       a,b,c = st.columns([0.2,0.6,0.2])
+# predict the target from all the input features
+    prediction = model.predict(pred_arr)
+    
+    if prediction == 0:
+        st.write(f"The severity prediction is fatal injury⚠")
+    elif prediction == 1:
+        st.write(f"The severity prediction is serious injury")
+    else:
+        st.write(f"The severity prediction is slight injury")
+    
+    st.write("Developed By: Avi kumar Talaviya")
+    st.markdown("""Reach out to me on: [Twitter](https://twitter.com/avikumart_) |
+    [Linkedin](https://www.linkedin.com/in/avi-kumar-talaviya-739153147/) |
+    [Kaggle](https://www.kaggle.com/avikumart) 
+    """)
+
+a,b,c = st.columns([0.2,0.6,0.2])
 with b:
- st.image("banner-picture.jpeg", use_column_width=True)
+st.image("banner-picture.jpeg", use_column_width=True)
 
 
 # description about the project and code files       
@@ -119,7 +119,7 @@ The metric for evaluation will be f1-score
 """)
 
 st.markdown("Please find GitHub repository link of project: [Click Here](https://github.com/avikumart/Road-Traffic-Severity-Classification-Project)")          
-  
+
 # run the main function        
 if __name__ == '__main__':
-  main()
+main()
